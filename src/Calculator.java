724023f8e99a665e142d1fcee7ae5fef6feadf70
Georgy <georgy.matskevich@gmail.com>
2022-04-06 01:00:59 +0300

    Second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,11 +6,11 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multyply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = //(x, y) -> x / y;
     // Проверяем y на ноль, в случае если ноль проводим хитрое выражение, которое
     // позволяем делить на ноль, т.к. если делить на 0 будет 0. Так же как и 0 делить на любое
     // другое число будет 0
-    //(x, y) -> y != 0 ? x / y : ((x / (y + 1)) / x) - 1;
+    (x, y) -> y != 0 ? x / y : ((x / (y + 1)) / x) - 1;
 
 
     UnaryOperator<Integer> pow = x -> x * x;
